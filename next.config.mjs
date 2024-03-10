f7188fee03934ae116dd62f66bb1ef0436d2bc0b
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-09 23:37:52 -0800

    fix image bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['static01.nyt.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'static01.nyt.com',
+            },
+        ]
+    },
+};
 
 export default nextConfig;
